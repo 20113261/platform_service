9c4e21daad6249faf30ecce20abb972764596cd7
hourong <nmghr9@gmail.com>
2017-03-22 11:25:41 +0800

    增加切分日志部分

--- proj/celery.py
+++ proj/celery.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import
 
 from celery import Celery
+from celery.signals import setup_logging
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Celery('proj', include=['proj.tasks',
                               'proj.hotel_tasks',
@@ -16,6 +19,21 @@ app = Celery('proj', include=['proj.tasks',
                               'proj.daodao_img_rename_tasks'
                               ])
 app.config_from_object('proj.config')
+
+
+def initialize_logger(loglevel=logging.INFO, **kwargs):
+    handler = RotatingFileHandler(
+        '/search/log/celery.log',
+        maxBytes=100 * 1024 * 1024,
+        backupCount=10
+    )
+    log = logging.getLogger('celery')
+    log.addHandler(handler)
+    log.setLevel(loglevel)
+    return log
+
+
+setup_logging.connect(initialize_logger)
 
 if __name__ == '__main__':
     app.start()
