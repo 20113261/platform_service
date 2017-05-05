16ae9e8a671e09750506cd6374478540661db995
hourong <hourong@hourongdeMacBook-Pro.local>
2017-05-05 19:16:25 +0800

    增加任务成功，失败任务记录

--- proj/my_lib/BaseTask.py
+++ proj/my_lib/BaseTask.py
@@ -1,11 +1,26 @@
 import pymysql
 import json
+import redis
+import socket
 from celery.task import Task
+
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    res = s.getsockname()[0]
+    s.close()
+    return res
 
 
 class BaseTask(Task):
     default_retry_delay = 1
     max_retries = 3
+
+    def on_success(self, retval, task_id, args, kwargs):
+        print 'on success called'
+        r = redis.Redis(host='10.10.180.145', db=3)
+        r.incr(self.name + '|_||_|' + get_local_ip() + '|_||_|success')
 
     def on_retry(self, exc, task_id, args, kwargs, einfo):
         conn = pymysql.connect(host='10.10.180.145', user='hourong', passwd='hourong', db='Task', charset='utf8')
@@ -19,6 +34,10 @@ class BaseTask(Task):
         conn.close()
 
     def on_failure(self, exc, task_id, args, kwargs, einfo):
+        print 'on failure called'
+        r = redis.Redis(host='10.10.180.145', db=3)
+        r.incr(self.name + '|_||_|' + get_local_ip() + '|_||_|failure')
+
         conn = pymysql.connect(host='10.10.180.145', user='hourong', passwd='hourong', db='Task', charset='utf8')
         with conn as cursor:
             celery_task_id = task_id
