4b5423c7f42da2ce738f9c33399fed880f874e0c
hourong <nmghr9@gmail.com>
2017-06-16 18:55:49 +0800

    设定页面部分为 Binary

--- proj/my_lib/PageSaver.py
+++ proj/my_lib/PageSaver.py
@@ -7,11 +7,12 @@
 # @Software: PyCharm
 import pymongo
 import zlib
+from bson import Binary
 
 
 def save_task_and_page_content(task_name, content, *args, **kwargs):
     client = pymongo.MongoClient(host='10.10.231.105', port=27017)
     collection = client['PageSaver'][task_name]
     data = zlib.compress(content.encode('utf8'))
-    kwargs['content'] = data
+    kwargs['content'] = Binary(data)
     collection.save(kwargs)
