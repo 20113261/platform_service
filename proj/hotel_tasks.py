5d8feb551d57c0c6006efa50aa24cafa69523baf
hourong <nmghr9@gmail.com>
2017-06-20 17:01:29 +0800

    关闭 FileSaver 打开 PageSaver

--- proj/hotel_tasks.py
+++ proj/hotel_tasks.py
@@ -52,9 +52,6 @@ def hotel_base_data(self, source, url, other_info, part, **kwargs):
         page.encoding = 'utf8'
         content = page.text
 
-        if kwargs.get('task_id'):
-            save_file(kwargs['task_id'], self.request.id, content)
-
         result = parse_hotel(content=content, url=url, other_info=other_info, source=source, part=part)
 
         if not result:
@@ -63,9 +60,9 @@ def hotel_base_data(self, source, url, other_info, part, **kwargs):
         else:
             if kwargs.get('task_id'):
                 # 保存抓取成功后的页面信息
-                # save_task_and_page_content(task_name=part, content=content, task_id=kwargs['task_id'], source=source,
-                #                            source_id=other_info['source_id'],
-                #                            city_id=other_info['city_id'], url=url)
+                save_task_and_page_content(task_name=part, content=content, task_id=kwargs['task_id'], source=source,
+                                           source_id=other_info['source_id'],
+                                           city_id=other_info['city_id'], url=url)
                 update_task(kwargs['task_id'])
             print "Success with " + PROXY + ' CODE 0'
             update_proxy('Platform', PROXY, x, '0')
