ef84afd2d3885791a85a1cb7b1e6caac26b26442
hourong <nmghr9@gmail.com>
2017-06-20 10:37:11 +0800

    增加取页面部分

--- proj/my_lib/PageSaver.py
+++ proj/my_lib/PageSaver.py
@@ -5,9 +5,17 @@
 # @Site    : 
 # @File    : PageSaver.py
 # @Software: PyCharm
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 import pymongo
 import zlib
+import time
 from bson import Binary
+
+DEFAULT_TASK_INFO_KEYS = ['source', 'source_id', 'city_id', 'url']
 
 
 def save_task_and_page_content(task_name, content, *args, **kwargs):
@@ -16,3 +24,25 @@ def save_task_and_page_content(task_name, content, *args, **kwargs):
     data = zlib.compress(content.encode('utf8'))
     kwargs['content'] = Binary(data)
     collection.save(kwargs)
+    client.close()
+
+
+def get_task_and_page_content(parent_task_id, task_name, **kwargs):
+    start = time.time()
+    client = pymongo.MongoClient(host='10.10.231.105', port=27017)
+    collection = client['PageSaver'][task_name]
+    res = collection.find_one({'task_id': parent_task_id})
+    if 'task_info_keys' in kwargs:
+        task_info_keys = kwargs['task_info_keys']
+    else:
+        task_info_keys = DEFAULT_TASK_INFO_KEYS
+
+    result = {k: res[k] for k in task_info_keys}
+    result['content'] = zlib.decompress(res['content']).decode()
+    client.close()
+    print 'Get Task Takes', time.time() - start
+    return result
+
+
+if __name__ == '__main__':
+    print get_task_and_page_content('7ededbc01f00e0463f064e6ca9f8235f', 'hotel_base_data_170612')
