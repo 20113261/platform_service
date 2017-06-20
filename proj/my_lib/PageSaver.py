81e0036f57a42839fac521b2e69fb96c5b1fd42b
hourong <nmghr9@gmail.com>
2017-06-20 16:52:16 +0800

    修改为存文件

--- proj/my_lib/PageSaver.py
+++ proj/my_lib/PageSaver.py
@@ -13,23 +13,35 @@ sys.setdefaultencoding('utf8')
 import pymongo
 import zlib
 import time
+import os
 from bson import Binary
 
 DEFAULT_TASK_INFO_KEYS = ['source', 'source_id', 'city_id', 'url']
 
+client = pymongo.MongoClient(host='10.10.231.105', port=27017)
 
-def save_task_and_page_content(task_name, content, *args, **kwargs):
-    client = pymongo.MongoClient(host='10.10.231.105', port=27017)
+
+def save_task_and_page_content_old(task_name, content, *args, **kwargs):
     collection = client['PageSaver'][task_name]
     data = zlib.compress(content.encode('utf8'))
     kwargs['content'] = Binary(data)
     collection.save(kwargs)
-    client.close()
+
+
+def save_task_and_page_content(task_name, content, *args, **kwargs):
+    collection = client['PageSaver'][task_name]
+    data = zlib.compress(content.encode('utf8'))
+    dir_path = os.path.join('/data/nfs/page_saver', task_name)
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+    f = open(os.path.join('/data/nfs/page_saver', task_name, kwargs['task_id']), 'wb')
+    f.write(data)
+    f.close()
+    collection.save(kwargs)
 
 
 def get_task_and_page_content(parent_task_id, task_name, **kwargs):
     start = time.time()
-    client = pymongo.MongoClient(host='10.10.231.105', port=27017)
     collection = client['PageSaver'][task_name]
     res = collection.find_one({'task_id': parent_task_id})
     if 'task_info_keys' in kwargs:
