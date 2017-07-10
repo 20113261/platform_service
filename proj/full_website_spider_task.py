cbf0fb4c7d9be59abe72f6d316abe6fb3cf952d7
hourong <nmghr9@gmail.com>
2017-07-10 10:21:17 +0800

    将静态数据 url 保存，防止 js css 等被多次抓取

--- proj/full_website_spider_task.py
+++ proj/full_website_spider_task.py
@@ -47,14 +47,21 @@ def full_site_spider(self, url, level, parent_url, parent_info, **kwargs):
 
                 # 分发新的任务
                 for next_url in next_url_set:
-                    if not urlSaver.has_crawled(parent_url, next_url):
-                        if level < MAX_LEVEL:
+                    if not (
+                                urlSaver.has_crawled(parent_url, next_url) or urlSaver.has_crawled('static_data',
+                                                                                                   next_url)
+                    ):
+                        if level < MAX_LEVEL - 1:
                             # full_site_spider.delay(next_url, level + 1, parent_url, parent_info, **kwargs)
                             app.send_task('proj.full_website_spider_task.full_site_spider',
                                           args=(next_url, level + 1, parent_url, parent_info,),
                                           kwargs=kwargs,
                                           queue='full_site_task',
                                           routing_key='full_site_task')
+
+            else:
+                # 将非 html 页面入 set 防止多次抓取，保存已抓取页面 url
+                urlSaver.add_url('static_data', url)
 
         except Exception as exc:
             session.update_proxy('23')
