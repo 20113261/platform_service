d854a80e29be5cabd422af30364b1161e1fa94ae
hourong <nmghr9@gmail.com>
2017-07-11 11:47:49 +0800

    修改已抓 url 过滤

--- proj/my_lib/Common/RedisUrlSaver.py
+++ proj/my_lib/Common/RedisUrlSaver.py
@@ -6,20 +6,32 @@
 # @File    : RedisUrlSaver.py
 # @Software: PyCharm
 import redis
+import urlparse
 
 
 class UrlSaver(object):
     def __init__(self):
-        self.r = redis.Redis(host='10.10.213.148', db=13)
+        self.real_url = redis.Redis(host='10.10.213.148', db=14)
+        self.unique_url = redis.Redis(host='10.10.213.148', db=15)
+
+    @staticmethod
+    def get_unique_url(url):
+        url_parse_result = urlparse.urlparse(url)
+        return url_parse_result.netloc + url_parse_result.path + '?' + '|'.join(
+            urlparse.parse_qs(url_parse_result.query))
 
     def add_url(self, key, url):
-        self.r.sadd(key, url)
+        self.real_url.sadd(key, url)
+        self.unique_url.sadd(key, self.get_unique_url(url))
 
     def has_crawled(self, key, url):
-        return self.r.sismember(key, url)
+        return self.unique_url.sismember(key, self.get_unique_url(url))
 
 
 if __name__ == '__main__':
     us = UrlSaver()
     print us.has_crawled(u'http://www.castillosanfernando.org/',
                          u'http://www.castillosanfernando.org/visita-virtual-castell-sant-ferran?id=185&vsig1_0=6')
+
+    print us.get_unique_url(
+        'http://www.zg-nadbiskupija.hr/nadbiskupija/dokumenti/svetista-i-prostenista/nadbiskupija/default.aspx?id=7237&2=3&ijk=bbk')
