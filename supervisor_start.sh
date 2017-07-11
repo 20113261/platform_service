8c5f29fedac933bc3d29e857f78f38a4f66bb04d
hourong <nmghr9@gmail.com>
2017-07-11 20:42:57 +0800

    分队列，修改默认任务和其他任务

--- supervisor_start.sh
+++ supervisor_start.sh
@@ -4,9 +4,16 @@ CURR_PATH=`cd $(dirname $0);pwd;`
 cd $CURR_PATH
 export PYTHONPATH='/data/lib'
 export PATH="$PATH:/usr/local/bin"
+export CONFIG_FILE="/data/lib/slave.spider.ini"
 #/usr/local/bin/celery worker -A proj --autoscal=5000,100 -P gevent -l info --logfile=/search/log/celery.log
 #/usr/local/bin/celery worker -A proj --autoscal=5000,100 -P gevent -l info
 #/usr/local/bin/celery worker -A proj --autoscal=80,5 -c 40 -l info
 #/usr/local/bin/celery worker -A proj -c 80 -l info
-/usr/local/bin/celery worker -A proj -P eventlet --autoscale=2000,200 -l info
+#
+#
+# Queue('hotel_suggestion', exchange=Exchange('hotel_suggestion', type='direct'), routing_key='hotel_suggestion'),
+#        Queue('full_site_task', exchange=Exchange('full_site_task', type='direct'), routing_key='full_site_task'),
+#        Queue('hotel_task', exchange=Exchange('hotel_task', type='direct'), routing_key='hotel_task'),
+#        Queue('hotel_list_task', exchange=Exchange('hotel_list_task', type='direct'), routing_key='hotel_list_task'),
+/usr/local/bin/celery worker -A proj -P eventlet --autoscale=2000,200 -Q hotel_suggestion,full_site_task,hotel_task -l info
 #/usr/local/bin/celery worker -A proj --autoscal=10,3 -l info --logfile=/search/log/celery.log
