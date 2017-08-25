d5d003612841297d2396fe0f40efd2b9131a2b21
nmghr9 <nmghr9@gmail.com>
2017-08-25 10:07:32 +0800

    修改日志样式

--- proj/my_lib/logger.py
+++ proj/my_lib/logger.py
@@ -30,8 +30,9 @@ def get_logger(logger_name):
     # getLogger 为单例模式
     api_logger = logging.getLogger(logger_name)
     api_logger.setLevel(logging.DEBUG)
-    file_log_format = """[%(asctime)s][%(name)s][line:%(lineno)d]%(levelname)s:  %(message)s"""
-    formtter = logging.Formatter(file_log_format)
+    datefmt = "%Y-%m-%d %H:%M:%S"
+    file_log_format = "%(asctime)-15s %(threadName)s %(filename)s:%(lineno)d %(levelname)s:        %(message)s"
+    formtter = logging.Formatter(file_log_format, datefmt)
 
     # handler 存在的判定
     if not api_logger.handlers:
