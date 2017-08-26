9a6fdf5624c317d70cfca0348c214a3a47f9fe2d
luwanning <luwanning@mioji.com>
2017-08-26 21:34:07 +0800

    经纬度信息获取不到，调整

--- proj/my_lib/new_hotel_parser/elong_parser.py
+++ proj/my_lib/new_hotel_parser/elong_parser.py
@@ -63,7 +63,7 @@ def elong_parser(content, url, other_info):
     try:
         lat = re.findall(r'"lat":"([-+\d\.]*)"', content)[0]
         lon = re.findall(r'"lon":"([-+\d\.]*)"', content)[0]
-        map_infos = map_pat.findall(content)[0]
+        # map_infos = map_pat.findall(content)[0]
         hotel.map_info = lat + ',' + lon
     except Exception, e:
         hotel.map_info = 'NULL'
