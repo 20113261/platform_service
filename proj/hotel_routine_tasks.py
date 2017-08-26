231a93ee8f616bdd642415405fc3686cd801a2cf
nmghr9 <nmghr9@gmail.com>
2017-08-27 00:15:52 +0800

    修改异常以及错误返回和日志

--- proj/hotel_routine_tasks.py
+++ proj/hotel_routine_tasks.py
@@ -13,10 +13,13 @@ from proj.my_lib.new_hotel_parser.hotel_parser import parse_hotel, TypeCheckErro
 from proj.my_lib.BaseRoutineTask import BaseRoutineTask
 from proj.my_lib.PageSaver import save_task_and_page_content
 from proj.my_lib.Common.Browser import MySession
+from proj.my_lib.logger import get_logger
+
+logger = get_logger("HotelDetail")
 
 
 @app.task(bind=True, base=BaseRoutineTask, max_retries=2, rate_limit='8/s')
-def hotel_routine_base_data(self, source, url, other_info, part, **kwargs):
+def hotel_routine_base_data(self, source, url, other_info, **kwargs):
     self.task_source = source.title()
     self.task_type = 'Hotel'
 
@@ -30,6 +33,7 @@ def hotel_routine_base_data(self, source, url, other_info, part, **kwargs):
             url = 'http://zh.hotels.com/hotel/details.html?hotel-id=' + hotel_id
     except Exception as e:
         self.error_code = 12
+        logger.exception()
         raise e
 
     # 修改请求参数
@@ -37,6 +41,7 @@ def hotel_routine_base_data(self, source, url, other_info, part, **kwargs):
         pass
     except Exception as e:
         self.error_code = 101
+        logger.exception()
         raise e
 
     try:
@@ -46,15 +51,18 @@ def hotel_routine_base_data(self, source, url, other_info, part, **kwargs):
         content = page.text
     except Exception as e:
         self.error_code = 22
+        logger.exception()
         raise e
 
     try:
         result = parse_hotel(content=content, url=url, other_info=other_info, source=source, part=part)
     except TypeCheckError as e:
         self.error_code = 102
+        logger.exception()
         raise e
     except Exception as e:
         self.error_code = 27
+        logger.exception()
         raise e
 
     try:
@@ -64,13 +72,16 @@ def hotel_routine_base_data(self, source, url, other_info, part, **kwargs):
         session.close()
     except Exception as e:
         self.error_code = 33
+        logger.exception()
         raise e
 
     try:
         # 保存抓取成功后的页面信息
-        save_task_and_page_content(task_name=part, content=content, task_id=kwargs['task_id'], source=source,
+        save_task_and_page_content(task_name='hotelinfo_routine_{0}'.format(source), content=content,
+                                   task_id=kwargs['task_id'], source=source,
                                    source_id=other_info['source_id'],
                                    city_id=other_info['city_id'], url=url)
     except Exception as e:
         self.error_code = 104
+        logger.exception()
         raise e
