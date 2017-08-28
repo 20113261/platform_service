f3ac76b70770868041dc7c13ad55b45bfcd023b3
luwanning <luwanning@mioji.com>
2017-08-28 21:13:44 +0800

    修改关联关系

--- proj/my_lib/new_hotel_parser/hotel_parser.py
+++ proj/my_lib/new_hotel_parser/hotel_parser.py
@@ -41,7 +41,7 @@ def parse_hotel(content, url, other_info, source, part):
     if not key_is_legal(result.map_info):
         raise TypeCheckError('Error map_info NULL        with parser %ss    url %s' % (parser.func_name, url))
 
-    if not (key_is_legal(result.hotel_name) or key_is_legal(result.hotel_name_en)):
+    if not key_is_legal(result.hotel_name) or not key_is_legal(result.hotel_name_en):
         raise TypeCheckError('Error hotel_name and hotel_name_en Both NULL        with parser %s    url %s' % (parser.func_name, url))
 
     if result.source == 'booking':
