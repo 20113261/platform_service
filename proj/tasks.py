bf31baf3e6fdbe1e2ebee0548fc7af3f8401eb50
luwanning <luwanning@mioji.com>
2017-08-30 20:52:59 +0800

    提升速度

--- proj/tasks.py
+++ proj/tasks.py
@@ -282,7 +282,7 @@ def get_lost_rest_no_proxy(self, target_url):
         self.retry(exc=traceback.format_exc(exc))
 
 
-@app.task(bind=True, base=BaseTask, max_retries=2, rate_limit='40/s')
+@app.task(bind=True, base=BaseTask, max_retries=2, rate_limit='60/s')
 def get_images(self, source, target_url, **kwargs):
     # PROXY = get_proxy(source="Platform")
     # x = time.time()
