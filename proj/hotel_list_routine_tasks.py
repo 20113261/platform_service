bc878e41ffeed1925b4fba29341a620dd13df076
luwanning <luwanning@mioji.com>
2017-09-12 12:04:43 +0800

    修改数据库提交逻辑

--- proj/hotel_list_routine_tasks.py
+++ proj/hotel_list_routine_tasks.py
@@ -100,13 +100,11 @@ def hotel_routine_list_task(self, source, city_id, check_in, **kwargs):
         cursor = conn.cursor()
         sql = "REPLACE INTO hotel_base_data_task (source, source_id, city_id, hotel_url) VALUES (%s,%s,%s,%s)"
         cursor.executemany(sql, data_res)
-        conn.commit()
         cursor.close()
+        conn.commit()
     except Exception as e:
         self.error_code = 33
         raise e
-    finally:
-        conn.close()
     return True
 
 
