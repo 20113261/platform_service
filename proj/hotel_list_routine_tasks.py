32e467f1e0f43d8f9a1abc5f2eec571759e54319
nmghr9 <nmghr9@gmail.com>
2017-09-12 14:36:48 +0800

    routine task modified

--- proj/hotel_list_routine_tasks.py
+++ proj/hotel_list_routine_tasks.py
@@ -100,8 +100,8 @@ def hotel_routine_list_task(self, source, city_id, check_in, **kwargs):
         cursor = conn.cursor()
         sql = "REPLACE INTO hotel_base_data_task (source, source_id, city_id, hotel_url) VALUES (%s,%s,%s,%s)"
         cursor.executemany(sql, data_res)
-        cursor.close()
         conn.commit()
+        cursor.close()
     except Exception as e:
         self.error_code = 33
         raise e
