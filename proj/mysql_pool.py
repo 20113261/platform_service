b085886735086bb0e313679a8a3d94235f03c437
nmghr9 <nmghr9@gmail.com>
2017-09-20 14:40:57 +0800

    增加 mysql pool

---
+++ proj/mysql_pool.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/9/20 下午2:40
+# @Author  : Hou Rong
+# @Site    : 
+# @File    : mysql_pool.py
+# @Software: PyCharm
+import mysql.connector
+
+db_config = dict(
+    user='mioji_admin',
+    password='mioji1109',
+    host='10.10.228.253',
+    database='ServicePlatform'
+)
+service_platform_conn = mysql.connector.connect(pool_name="service-platform-pool",
+                                                pool_size=15,
+                                                **db_config)
