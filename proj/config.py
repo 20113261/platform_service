b736be72f22c4ecc3e7c66641440c99350221d9c
nmghr9 <nmghr9@gmail.com>
2017-09-23 19:07:22 +0800

    update mongo task backend

--- proj/config.py
+++ proj/config.py
@@ -5,13 +5,19 @@ from __future__ import absolute_import
 # CELERY_RESULT_BACKEND = 'amqp://hourong:1220@10.10.189.213/celery'
 BROKER_URL = 'amqp://hourong:1220@10.10.189.213/celery'
 
-CELERY_RESULT_BACKEND = "mongodb"
+CELERY_RESULT_BACKEND = 'mongodb://10.10.231.105:27017/'
+
 CELERY_MONGODB_BACKEND_SETTINGS = {
-    "host": "10.10.231.105",
-    "port": 27017,
-    "database": "Backend",
-    "store_task": "store_task",
+    'database': 'Backend',
+    'taskmeta_collection': 'CeleryTask',
 }
+# CELERY_RESULT_BACKEND = "mongodb"
+# CELERY_MONGODB_BACKEND_SETTINGS = {
+#     "host": "10.10.231.105",
+#     "port": 27017,
+#     "database": "Backend",
+#     "store_task": "store_task",
+# }
 # BROKER_URL = 'redis://127.0.0.1:6379/10'
 from kombu import Queue, Exchange
 
