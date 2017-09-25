0204f35de666ae3c438cb6ea8287ea56630483c0
nmghr9 <nmghr9@gmail.com>
2017-09-25 12:01:02 +0800

    poi 任务错误码修改

--- proj/poi_task.py
+++ proj/poi_task.py
@@ -36,26 +36,24 @@ def get_lost_poi(self, target_url, city_id, poi_type, country_id, **kwargs):
         page = session.get(target_url, timeout=15)
         page.encoding = 'utf8'
         result = parser_type[poi_type](page.content, target_url, city_id=city_id)
-        save_task_and_page_content(task_name='daodao_poi_' + poi_type, content=page.content,
-                                   task_id=kwargs['mongo_task_id'],
-                                   source='daodao',
-                                   source_id='NULL',
-                                   city_id='NULL', url=target_url)
 
         if result == 'Error':
-            raise Exception, 'parse %s Error' % target_url
+            self.error_code = 27
+            raise Exception('parse %s Error' % target_url)
 
-        if not result['imgurl']:
-            raise Exception('zhao bu dao tupian')
-        # rest_insert_db(result, city_id)
         result['city_id'] = city_id
         result['utime'] = datetime.datetime.now()
         sql_key = result.keys()
 
-        session = DBSession()
-        session.execute(text(text_2_sql(sql_key).format(table_name=kwargs['task_name'])), [result])
-        session.commit()
-        session.close()
+        try:
+            session = DBSession()
+            session.execute(text(text_2_sql(sql_key).format(table_name=kwargs['task_name'])), [result])
+            session.commit()
+            session.close()
+        except Exception as e:
+            self.error_code = 33
+            logger.exception(e)
+            raise e
 
         self.error_code = 0
         return result
