817c0f791e654dead0d86c1bddce75fc5a0db0ef
nmghr9 <nmghr9@gmail.com>
2017-09-26 12:15:59 +0800

    酒店详情页测试

---
+++ test/hotel_detail_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/9/22 下午6:39
+# @Author  : Hou Rong
+# @Site    : 
+# @File    : hotel_detail_test.py
+# @Software: PyCharm
+from proj.hotel_tasks import hotel_base_data
+
+if __name__ == '__main__':
+    hotel_base_data(source='booking',
+                    url='http://www.booking.com/hotel/ma/assilah-marina-golf-assilah12.zh-cn.html?label=gen173nr-1FCAEoggJCAlhYSDNiBW5vcmVmcgV1c19jYYgBAZgBMsIBA2FibsgBDNgBAegBAfgBC5ICAXmoAgQ;sid=050b6a05c88840f3fd54bb8b9bf8cb62;checkin=2017-09-22;checkout=2017-09-23;ucfs=1;soh=1;srpvid=0f7c3351e00606b6;srepoch=1506064675;highlighted_blocks=;all_sr_blocks=;room1=A%2CA;soldout=0%2C0;hpos=12;hapos=72;dest_type=city;dest_id=-24680;srfid=15119f6b66615aadc25111e98e76dc3ad45ae212X72;from=searchresults;highlight_room=#no_availability_msg',
+                    part='detail_hotel_booking_20170919a', other_info={
+            'source_id': '2531377', 'city_id': '51511'
+        }, country_id='NULL', task_name='detail_hotel_booking_20170919a', retry_count=0, max_retry_times=10)
