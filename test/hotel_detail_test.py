1ca19aafa5ebb171a1cff093be34499fa865d76a
nmghr9 <nmghr9@gmail.com>
2017-09-27 13:07:59 +0800

    detail test

--- test/hotel_detail_test.py
+++ test/hotel_detail_test.py
@@ -14,10 +14,30 @@ if __name__ == '__main__':
     #         'source_id': '2531377', 'city_id': '51511'
     #     }, country_id='NULL', task_name='detail_hotel_booking_20170919a', retry_count=0, max_retry_times=10)
 
+    # hotel_base_data(source='hotels',
+    #                 url='http://zh.hotels.com/ho381063/?pa=8&q-check-out=2017-12-16&tab=description&q-room-0-adults=2&YGF=14&q-check-in=2017-12-15&MGT=1&WOE=6&WOD=5&ZSX=0&SYE=3&q-room-0-children=0',
+    #                 part='detail_hotel_hotels_20170925d', other_info={
+    #         'source_id': '381063', 'city_id': 'NULL'
+    #     },
+    #                 country_id='NULL',
+    #                 task_name='detail_hotel_hotels_20170925d',
+    #                 retry_count=0,
+    #                 max_retry_times=10)
+
+    # hotel_base_data(source='hotels',
+    #                 url='https://zh.hotels.com/ho677996/',
+    #                 part='detail_hotel_hotels_20170925d', other_info={
+    #         'source_id': '677996', 'city_id': 'NULL'
+    #     },
+    #                 country_id='NULL',
+    #                 task_name='detail_hotel_hotels_20170925d',
+    #                 retry_count=0,
+    #                 max_retry_times=10)
+
     hotel_base_data(source='hotels',
-                    url='http://zh.hotels.com/ho381063/?pa=8&q-check-out=2017-12-16&tab=description&q-room-0-adults=2&YGF=14&q-check-in=2017-12-15&MGT=1&WOE=6&WOD=5&ZSX=0&SYE=3&q-room-0-children=0',
+                    url='http://zh.hotels.com/ho463739/?pa=1&q-check-out=2017-12-06&tab=description&q-room-0-adults=2&YGF=14&q-check-in=2017-12-05&MGT=1&WOE=3&WOD=2&ZSX=0&SYE=3&q-room-0-children=0',
                     part='detail_hotel_hotels_20170925d', other_info={
-            'source_id': '381063', 'city_id': 'NULL'
+            'source_id': '463739', 'city_id': 'NULL'
         },
                     country_id='NULL',
                     task_name='detail_hotel_hotels_20170925d',
