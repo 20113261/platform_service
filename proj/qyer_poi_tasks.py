65a30bc347d93b9783da515bc4619b48b2eef98d
nmghr9 <nmghr9@gmail.com>
2017-09-27 15:20:47 +0800

    qyer 详情页解析修改
    1、google address 添加
    2、日志
    3、关键字段解析

--- proj/qyer_poi_tasks.py
+++ proj/qyer_poi_tasks.py
@@ -14,6 +14,12 @@ from proj.my_lib.BaseTask import BaseTask
 from proj.my_lib.Common.Browser import MySession
 from proj.my_lib.new_hotel_parser.data_obj import text_2_sql
 from sqlalchemy.sql import text
+from proj.my_lib.logger import get_logger
+from proj.my_lib.ServiceStandardError import TypeCheckError
+from proj.my_lib.Common.KeyMatch import key_is_legal
+from proj.my_lib.Common.Utils import google_get_map_info
+
+logger = get_logger("QyerPoiDetail")
 
 
 @app.task(bind=True, base=BaseTask, max_retries=3, rate_limit='6/s')
@@ -27,28 +33,51 @@ def qyer_poi_task(self, target_url, city_id, **kwargs):
         content = page.text
         result = page_parser(content=content, target_url=target_url)
         result.city_id = city_id
-        if result.name == 'NULL' and result.name_en == 'NULL':
-            self.error_code = 102
-            raise Exception("name and name_en all NULL")
-        elif result.map_info == 'NULL':
-            self.error_code = 102
-            raise Exception('map_info is NULL')
+
+        retry_count = kwargs['retry_count']
+
+        name = result.name
+        name_en = result.name_en
+        map_info = result.map_info
+        address = result.address
+
+        if not key_is_legal(map_info):
+            if retry_count > 5:
+                if not key_is_legal(address):
+                    raise TypeCheckError(
+                        'Error map_info and address NULL        with parser %ss    url %s' % (
+                            page_parser.func_name, target_url))
+                google_map_info = google_get_map_info(address)
+                if not key_is_legal(google_map_info):
+                    raise TypeCheckError(
+                        'Error google_map_info  NULL        with parser %ss    url %s' % (
+                            page_parser.func_name, target_url))
+                result['address'] = google_map_info
+            else:
+                raise TypeCheckError(
+                    'Error map_info NULL        with parser %ss    url %s' % (page_parser.func_name, target_url))
+
+        if key_is_legal(name) or key_is_legal(name_en):
+            logger.info(name + '  ----------  ' + name_en)
         else:
+            raise TypeCheckError(
+                'Error name and name_en Both NULL        with parser %s    url %s' % (
+                    page_parser.func_name, target_url))
 
-            sql_result = result.__dict__
-            sql_key = sql_result.keys()
-            if '_sa_instance_state' in sql_key:
-                sql_key.remove('_sa_instance_state')
+        sql_result = result.__dict__
+        sql_key = sql_result.keys()
+        if '_sa_instance_state' in sql_key:
+            sql_key.remove('_sa_instance_state')
 
-            try:
-                session = DBSession()
-                # session.merge(result)
-                session.execute(text(text_2_sql(sql_key).format(table_name=kwargs['task_name'])), [sql_result])
-                session.commit()
-                session.close()
-            except Exception as e:
-                self.error_code = 33
-                raise e
+        try:
+            session = DBSession()
+            # session.merge(result)
+            session.execute(text(text_2_sql(sql_key).format(table_name=kwargs['task_name'])), [sql_result])
+            session.commit()
+            session.close()
+        except Exception as e:
+            self.error_code = 33
+            raise e
 
         self.error_code = 0
         return self.error_code
