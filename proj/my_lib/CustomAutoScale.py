dcea2681ab9be5dc2c98b3a0326c47ef3f36b4c4
nmghr9 <nmghr9@gmail.com>
2017-10-06 20:42:47 +0800

    增加日志，修改上限

--- proj/my_lib/CustomAutoScale.py
+++ proj/my_lib/CustomAutoScale.py
@@ -7,6 +7,9 @@
 # @Software: PyCharm
 import psutil
 from celery.worker.autoscale import Autoscaler
+from proj.my_lib.logger import get_logger
+
+logger = get_logger('auto scale logger')
 
 
 class CustomAutoScale(Autoscaler):
@@ -14,15 +17,21 @@ class CustomAutoScale(Autoscaler):
         memory_obj = psutil.virtual_memory()
         memory_percent = memory_obj.percent
         procs = self.processes
-        if memory_percent < 90.0:
+        if memory_percent < 85.0:
             cur = min(self.qty, self.max_concurrency)
             if cur > procs:
                 self.scale_up(cur - procs)
+                logger.debug("[memory_percent: {}][current: {}][scale up: {}]".
+                             format(memory_percent, self.processes, cur - procs))
                 return True
             cur = max(self.qty, self.min_concurrency)
             if cur < procs:
                 self.scale_down(procs - cur)
-                return True
+                logger.debug("[memory_percent: {}][current: {}][scale down: {}]".
+                             format(memory_percent, self.processes, procs - cur))
+                return True 
         else:
             self.scale_down(1)
+            logger.debug("[memory_percent: {}][current: {}][scale down: {}]".
+                         format(memory_percent, self.processes, 1))
             return True
