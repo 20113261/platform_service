8065edf12a800078dbb96ead5bd453117a137a84
nmghr9 <nmghr9@gmail.com>
2017-10-06 20:51:43 +0800

    日志添加 worker_name

--- proj/my_lib/CustomAutoScale.py
+++ proj/my_lib/CustomAutoScale.py
@@ -14,6 +14,7 @@ logger = get_logger('auto scale logger')
 
 class CustomAutoScale(Autoscaler):
     def _maybe_scale(self, req=None):
+        worker_name = self.worker.hostname
         memory_obj = psutil.virtual_memory()
         memory_percent = memory_obj.percent
         procs = self.processes
@@ -21,17 +22,17 @@ class CustomAutoScale(Autoscaler):
             cur = min(self.qty, self.max_concurrency)
             if cur > procs:
                 self.scale_up(cur - procs)
-                logger.debug("[memory_percent: {}][current: {}][scale up: {}]".
-                             format(memory_percent, self.processes, cur - procs))
+                logger.debug("[worker_name: {}][memory_percent: {}][current: {}][scale up: {}]".
+                             format(worker_name, memory_percent, self.processes, cur - procs))
                 return True
             cur = max(self.qty, self.min_concurrency)
             if cur < procs:
                 self.scale_down(procs - cur)
-                logger.debug("[memory_percent: {}][current: {}][scale down: {}]".
-                             format(memory_percent, self.processes, procs - cur))
+                logger.debug("[worker_name: {}][memory_percent: {}][current: {}][scale down: {}]".
+                             format(worker_name, memory_percent, self.processes, procs - cur))
                 return True
         else:
             self.scale_down(1)
-            logger.debug("[memory_percent: {}][current: {}][scale down: {}]".
-                         format(memory_percent, self.processes, 1))
+            logger.debug("[worker_name: {}][memory_percent: {}][current: {}][scale down: {}]".
+                         format(worker_name, memory_percent, self.processes, 1))
             return True
