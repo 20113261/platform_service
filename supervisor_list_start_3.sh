bdd4214dbcf86e2140656074ba0322dcc4632b9e
nmghr9 <nmghr9@gmail.com>
2017-10-06 19:02:21 +0800

    提升列表页并发

---
+++ supervisor_list_start_3.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+CURR_PATH=`cd $(dirname $0);pwd;`
+cd $CURR_PATH
+export PYTHONPATH='/data/lib'
+export PATH="$PATH:/usr/local/bin"
+export CONFIG_FILE="/data/lib/slave.spider.ini"
+export CELERY_LOG_NAME="list_3"
+/usr/local/bin/celery worker -A proj -P gevent --autoscale=1500,30 -Q hotel_list,poi_list -l info
