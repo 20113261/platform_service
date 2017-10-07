fc45d7a7ff563b99224573b73ef855a08af7abcd
nmghr9 <nmghr9@gmail.com>
2017-10-07 19:09:18 +0800

    更换 supervisor 配置

---
+++ service_script/supervisord_service_script.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# Source function library
+. /etc/rc.d/init.d/functions
+
+# Source system settings
+if [ -f /etc/sysconfig/supervisord ]; then
+    . /etc/sysconfig/supervisord
+fi
+
+# Path to the supervisorctl script, server binary,
+# and short-form for messages.
+supervisorctl=/usr/local/bin/supervisorctl
+supervisord=${SUPERVISORD-/usr/local/bin/supervisord}
+prog=supervisord
+pidfile=${PIDFILE-/var/run/supervisord.pid}
+lockfile=${LOCKFILE-/var/lock/subsys/supervisord}
+STOP_TIMEOUT=${STOP_TIMEOUT-60}
+OPTIONS="${OPTIONS--c /etc/supervisord.conf}"
+RETVAL=0
+
+start() {
+    echo -n $"Starting $prog: "
+    daemon $supervisord $OPTIONS
+    RETVAL=$?
+    echo
+    if [ $RETVAL -eq 0 ]; then
+        touch ${lockfile}
+        $supervisorctl $OPTIONS status
+    fi
+    return $RETVAL
+}
+
+stop() {
+    echo -n $"Stopping $prog: "
+    killproc -d ${STOP_TIMEOUT} $supervisord
+    RETVAL=$?
+    echo
+    [ $RETVAL -eq 0 ] && rm -rf ${lockfile} ${pidfile}
+}
+
+restart() {
+    stop
+    start
+}
+
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    status)
+        status -p ${pidfile} $supervisord
+        RETVAL=$?
+        [ $RETVAL -eq 0 ] && $supervisorctl $OPTIONS status
+        ;;
+    restart|force-reload|reload)
+        restart
+        ;;
+    condrestart|try-restart)
+        if status -p ${pidfile} $supervisord >&/dev/null; then
+          stop
+          start
+        fi
+        ;;
+    *)
+        echo $"Usage: $prog {start|stop|restart|condrestart|try-restart|force-reload|reload}"
+        RETVAL=2
+esac
+
+exit $RETVAL
