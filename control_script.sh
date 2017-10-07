21fce025755f04aafacbfa76cfd664ff0e20f522
nmghr9 <nmghr9@gmail.com>
2017-10-07 12:13:34 +0800

    重启服务修改为温和的重启方式

--- control_script.sh
+++ control_script.sh
@@ -5,3 +5,12 @@ cat hosts.txt|xargs -I host scp supervisord.conf host:/etc
 
 # get free memory
 pssh -h hosts.txt -i 'cat /proc/meminfo |grep MemFree'
+
+# restart all running process
+ps -aux|grep "celery worker"|grep -v grep|awk '{print $2}'|xargs -I each_pid kill -HUP each_pid
+
+# get all running process pid
+/usr/bin/pssh -h /root/hosts.txt -i "ps -aux|grep 'celery worker'|grep -v grep|awk '{print \$2}'"
+
+# restart all running process from master
+/usr/bin/pssh -h /root/hosts.txt -i "ps -aux|grep 'celery worker'|grep -v grep|awk '{print \$2}'|xargs -I each_pid kill -HUP each_pid"
