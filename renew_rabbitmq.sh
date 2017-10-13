14d5fd64674b3a4687136079f587a30d0ce00d85
nmghr9 <nmghr9@gmail.com>
2017-10-13 11:38:46 +0800

    增加两个队列

--- renew_rabbitmq.sh
+++ renew_rabbitmq.sh
@@ -4,3 +4,9 @@
 /usr/sbin/rabbitmqctl set_user_tags hourong administrator
 /usr/sbin/rabbitmqctl add_vhost celery
 /usr/sbin/rabbitmqctl set_permissions -p celery hourong ".*" ".*" ".*"
+
+/usr/sbin/rabbitmqctl add_vhost data_insert
+/usr/sbin/rabbitmqctl set_permissions -p data_insert hourong ".*" ".*" ".*"
+
+/usr/sbin/rabbitmqctl add_vhost task_info
+/usr/sbin/rabbitmqctl set_permissions -p task_info hourong ".*" ".*" ".*"
