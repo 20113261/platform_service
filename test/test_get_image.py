cfeeeed59fcc210ea2a30693d2df3ba2e4ca49a2
nmghr9 <nmghr9@gmail.com>
2017-10-14 15:22:01 +0800

    提速

--- test/test_get_image.py
+++ test/test_get_image.py
@@ -24,17 +24,47 @@ if __name__ == '__main__':
     #     }
     # )
 
+    # get_images(
+    #     **{
+    #         'source': "daodao",
+    #         'new_part': "detail_attr_daodao_20171010a",
+    #         'target_url': "https://ccm.ddcdn.com/ext/photo-s/0e/10/3c/6e/the-much-photographed.jpg",
+    #         'desc_path': "/data/nfs/image/img_attr_daodao_20171010a_filter",
+    #         'is_poi_task': True,
+    #         'source_id': "7753114",
+    #         'part': "20171010a",
+    #         'file_path': "/data/nfs/image/img_attr_daodao_20171010a",
+    #         "task_name": "images_hotel_booking_20170929a",
+    #         "task_response": TaskResponse()
+    #     }
+    # )
+
+    # get_images(
+    #     **{
+    #         'source': "booking",
+    #         'new_part': "detail_hotel_booking_20170929a",
+    #         'target_url': "https://s-ec.bstatic.com/images/hotel/max1024x768/337/33766112.jpg",
+    #         'desc_path': "/data/nfs/image/img_hotel_booking_20170929a_filter",
+    #         'is_poi_task': False,
+    #         'source_id': "76675",
+    #         'part': "20170929a",
+    #         'file_path': "/data/nfs/image/img_hotel_booking_20170929a",
+    #         "task_name": "images_hotel_booking_20170929a",
+    #         "task_response": TaskResponse()
+    #     }
+    # )
+
     get_images(
         **{
             'source': "daodao",
-            'new_part': "detail_attr_daodao_20171010a",
-            'target_url': "https://ccm.ddcdn.com/ext/photo-s/0e/10/3c/6e/the-much-photographed.jpg",
-            'desc_path': "/data/nfs/image/img_attr_daodao_20171010a_filter",
+            'new_part': "detail_rest_daodao_20170928a",
+            'target_url': "https://ccm.ddcdn.com/ext/photo-w/05/9d/be/f4/spaghetti-ai-ricci-di.jpg",
+            'desc_path': "/data/nfs/image/img_rest_daodao_20170928a_filter",
             'is_poi_task': True,
-            'source_id': "7753114",
-            'part': "20171010a",
-            'file_path': "/data/nfs/image/img_attr_daodao_20171010a",
-            "task_name": "images_hotel_booking_20170929a",
+            'source_id': "4697785",
+            'part': "20170928a",
+            'file_path': "/data/nfs/image/img_rest_daodao_20170928a",
+            "task_name": "images_rest_daodao_20170928a",
             "task_response": TaskResponse()
         }
     )
