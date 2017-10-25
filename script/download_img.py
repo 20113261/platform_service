0f39f1a8c32233a0ad85318a185b78294c420de7
nmghr9 <nmghr9@gmail.com>
2017-10-25 18:14:41 +0800

    对于 2、每 30 秒从任务表中，按照城市 grade 优先选取前面的 1000 条没下载的图片放到指定位置，并入 PoiPictureInformation 中 进行了修改
    - 保证 PoiPictureInformation 中未扫描的图片数目为 3000 以上，3000 以下进行下载并添加相关信息操作。
    - 确保城市，poi 完整下载，按照 grade，city_id，poi_id 进行排序，每次入图片数加和首次大于 1000 的全部 poi 的图片

--- script/download_img.py
+++ script/download_img.py
@@ -21,29 +21,34 @@ pool = gevent.pool.Pool(size=200)
 logger = get_logger("pic_detect_download")
 
 PARENT_PATH = "/data/image/formatted_image"
-EACH_TIMES_DOWNLOAD = 1000
+
+SCAN_FILTER = 3000
+EACH_TIMES_PER_TASK = 1000
+MAX_PIC_PER_VIEW = 10000
+
+update_data_list = []
+task_data_list = []
 
 
 @func_time_logger
-def download_and_update_table(file_name, parent_path, poi_id, task_id):
-    download("mioji-attr", file_name, parent_path)
-    logger.debug("[download pic finished][task_id: {}][poi_id: {}][file name: {}]".format(task_id, poi_id, file_name))
-
-    # update task table status
+def insert_all_data():
+    global update_data_list
+    global task_data_list
+    # init insert conn and cursor
     task_conn = service_platform_pool.connection()
     task_cursor = task_conn.cursor()
-    task_cursor.execute('''UPDATE pic_detect_task SET status=2 WHERE id=%s;''', (task_id,))
-    logger.debug("[update status finished][task_id: {}][poi_id: {}][file name: {}]".format(task_id, poi_id, file_name))
 
-    # insert poi detect data table
     data_conn = spider_data_poi_pool.connection()
     data_cursor = data_conn.cursor()
-    data = (poi_id, os.path.join(parent_path.replace('/data/image/formatted_image', '/search/image/formatted_image'),
-                                 file_name))
-    data_cursor.execute(
-        '''INSERT IGNORE INTO PoiPictureInformation (poi_id, pic_name, is_scaned) VALUES (%s, %s, 0); ''',
-        data)
-    logger.debug("[update status finished][task_id: {}][poi_id: {}][file name: {}]".format(task_id, poi_id, file_name))
+
+    # update task table and insert data table
+    _res = task_cursor.executemany('''UPDATE pic_detect_task SET status=2 WHERE id=%s;''', update_data_list)
+    logger.debug("[update status finished][update task: {}]".format(_res))
+    _res = data_cursor.executemany(
+        '''INSERT IGNORE INTO PoiPictureInformation (city_id, poi_id, pic_name, is_scaned) VALUES (%s, %s, %s, 0); ''',
+        task_data_list)
+    logger.debug(
+        "[update status finished][insert poi face detect task: {}]".format(_res))
 
     # 由于事务关系原因，当两个 sql 均运行成功后才入库，否则不 commit
     task_conn.commit()
@@ -55,7 +60,26 @@ def download_and_update_table(file_name, parent_path, poi_id, task_id):
     data_conn.close()
 
 
-def download_pic():
+@func_time_logger
+def download_and_prepare_data(file_name, parent_path, city_id, poi_id, task_id):
+    global update_data_list
+    global task_data_list
+    download("mioji-attr", file_name, parent_path)
+    logger.debug("[download pic finished][task_id: {}][poi_id: {}][file name: {}]".format(task_id, poi_id, file_name))
+
+    # update task table status
+    update_data = (task_id,)
+    task_data = (
+        city_id, poi_id,
+        os.path.join(parent_path.replace('/data/image/formatted_image', '/search/image/formatted_image'),
+                     file_name))
+    update_data_list.append(update_data)
+    task_data_list.append(task_data)
+
+
+def _download_pic():
+    global update_data_list
+    global task_data_list
     conn = pymysql.connect(host='10.10.228.253', user='mioji_admin', password='mioji1109', charset='utf8',
                            db='ServicePlatform')
     cursor = conn.cursor()
@@ -64,20 +88,54 @@ def download_pic():
   city_id,
   poi_id,
   pic_name
-FROM pic_detect_task WHERE status=0 ORDER BY city_grade LIMIT {};'''.format(EACH_TIMES_DOWNLOAD))
+FROM pic_detect_task WHERE status=0 ORDER BY `city_grade`,`city_id`,`poi_id` LIMIT {};'''.format(MAX_PIC_PER_VIEW))
     start = time.time()
     _count = 0
+
+    update_data_list = []
+    task_data_list = []
+    old_poi_id = None
     for task_id, city_id, poi_id, file_name in cursor.fetchall():
         _count += 1
         city_path = os.path.join(PARENT_PATH, city_id)
         parent_path = os.path.join(city_path, poi_id)
         if not os.path.exists(parent_path):
             os.makedirs(parent_path)
-        pool.apply_async(download_and_update_table, (file_name, parent_path, poi_id, task_id))
+
+        if old_poi_id != poi_id:
+            old_poi_id = poi_id
+            logger.info("[next poi][city_id: {}][poi_id: {}][_count: {}]".format(city_id, poi_id, _count))
+            if _count > EACH_TIMES_PER_TASK:
+                break
+
+        pool.apply_async(download_and_prepare_data,
+                         (file_name, parent_path, city_id, poi_id, task_id))
     pool.join()
     cursor.close()
     conn.close()
+    insert_all_data()
     logger.debug("[Total: {}][Takes: {}]".format(_count, time.time() - start))
+
+
+def un_scanned_pic_count():
+    conn = spider_data_poi_pool.connection()
+    cursor = conn.cursor()
+    cursor.execute('''SELECT count(*)
+FROM PoiPictureInformation WHERE is_scaned=0;''')
+    _res = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
+    return int(_res[0])
+
+
+def download_pic():
+    _count = un_scanned_pic_count()
+    if _count > SCAN_FILTER:
+        logger.debug("[un scanned pic larger than: {}][count: {}]".format(SCAN_FILTER, _count))
+    else:
+        logger.debug("[start download pic][un scanned pic: {}]".format(_count))
+        _download_pic()
 
 
 if __name__ == '__main__':
