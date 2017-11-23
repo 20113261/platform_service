6b3db4729481c32c8cc293921d755500dec72442
nmghr9 <nmghr9@gmail.com>
2017-11-23 21:57:25 +0800

    回滚错误任务

--- task_query.js
+++ task_query.js
@@ -2404,3 +2404,109 @@ db.Task_Queue_poi_list_TaskName_list_total_qyer_20171120a.update({}, {
         'used_times': 0
     }
 }, false, true);
+
+
+db.Task_Queue_hotel_list_TaskName_list_hotel_hotels_20171122a.update({}, {
+    $set: {
+        'finished': 0,
+        'running': 0,
+        'used_times': 0
+    }
+}, false, true);
+
+db.City_Queue_hotel_list_TaskName_city_hotel_hotels_20171122a.update({}, {
+    $set: {
+        'finished': 0,
+        'data_count': [],
+        'task_result': [],
+        'date_index': 0
+    }
+}, false, true);
+
+
+
+db.Task_Queue_hotel_list_TaskName_list_hotel_booking_20171122a.update({}, {
+    $set: {
+        'finished': 0,
+        'running': 0,
+        'used_times': 0
+    }
+}, false, true);
+
+db.City_Queue_hotel_list_TaskName_city_hotel_booking_20171122a.update({}, {
+    $set: {
+        'finished': 0,
+        'data_count': [],
+        'task_result': [],
+        'date_index': 0
+    }
+}, false, true);
+
+
+db.Task_Queue_hotel_list_TaskName_list_hotel_ctrip_20171122a.update({}, {
+    $set: {
+        'finished': 0,
+        'running': 0,
+        'used_times': 0
+    }
+}, false, true);
+db.City_Queue_hotel_list_TaskName_city_hotel_ctrip_20171122a.update({}, {
+    $set: {
+        'finished': 0,
+        'data_count': [],
+        'task_result': [],
+        'date_index': 0
+    }
+}, false, true);
+
+
+db.Task_Queue_hotel_list_TaskName_list_hotel_expedia_20171122a.update({}, {
+    $set: {
+        'finished': 0,
+        'running': 0,
+        'used_times': 0
+    }
+}, false, true);
+db.City_Queue_hotel_list_TaskName_city_hotel_expedia_20171122a.update({}, {
+    $set: {
+        'finished': 0,
+        'data_count': [],
+        'task_result': [],
+        'date_index': 0
+    }
+}, false, true);
+
+
+db.Task_Queue_hotel_list_TaskName_list_hotel_elong_20171122a.update({}, {
+    $set: {
+        'finished': 0,
+        'running': 0,
+        'used_times': 0
+    }
+}, false, true);
+db.City_Queue_hotel_list_TaskName_city_hotel_elong_20171122a.update({}, {
+    $set: {
+        'finished': 0,
+        'data_count': [],
+        'task_result': [],
+        'date_index': 0
+    }
+}, false, true);
+
+
+db.Task_Queue_hotel_list_TaskName_list_hotel_agoda_20171122a.update({}, {
+    $set: {
+        'finished': 0,
+        'running': 0,
+        'used_times': 0
+    }
+}, false, true);
+db.City_Queue_hotel_list_TaskName_city_hotel_agoda_20171122a.update({}, {
+    $set: {
+        'finished': 0,
+        'data_count': [],
+        'task_result': [],
+        'date_index': 0
+    }
+}, false, true);
+
