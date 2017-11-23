5fc5f7f173d43b313fbf0ed536cdae0971dc9b86
nmghr9 <nmghr9@gmail.com>
2017-11-23 18:58:12 +0800

    test case

--- test/poi_list_test.py
+++ test/poi_list_test.py
@@ -9,11 +9,24 @@ from proj.my_lib.Common.Task import Task
 from proj.total_tasks import poi_list_task
 
 if __name__ == '__main__':
+    # task = Task(_worker='', _task_id='demo', _source='daodao', _type='rest', _task_name='list_rest_daodao_20170925d',
+    #             _used_times=0, max_retry_times=6,
+    #             kwargs={'source': 'daodao',
+    #                     'url': '/Tourism-g488103-Grand_Baie-Vacations.html',
+    #                     'city_id': '51513',
+    #                     'country_id': '409',
+    #                     'poi_type': 'rest'})
+    # poi_list_task(task=task)
+
     task = Task(_worker='', _task_id='demo', _source='daodao', _type='rest', _task_name='list_rest_daodao_20170925d',
                 _used_times=0, max_retry_times=6,
-                kwargs={'source': 'daodao',
-                        'url': '/Tourism-g488103-Grand_Baie-Vacations.html',
-                        'city_id': '51513',
-                        'country_id': '409',
-                        'poi_type': 'rest'})
+                kwargs={
+                    'source': 'daodao',
+                    'url': '/Tourism-g294261-Cebu_Island_Visayas-Vacations.html',
+                    'city_id': '20371',
+                    'country_id': '107',
+                    'poi_type': 'attr',
+                    'check_in': '20171203',
+                    'date_index': 0
+                })
     poi_list_task(task=task)
