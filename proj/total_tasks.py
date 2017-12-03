fa39db184bf4c193f0636cc2113e53fc05e5a9e9
nmghr9 <nmghr9@gmail.com>
2017-12-03 11:32:25 +0800

    qyer 详情页限速

--- proj/total_tasks.py
+++ proj/total_tasks.py
@@ -35,7 +35,7 @@ def hotel_list_task(self, task, **kwargs):
     return _sdk.execute()
 
 
-@app.task(bind=True, base=BaseTask, max_retries=3, rate_limit='1/s')
+@app.task(bind=True, base=BaseTask, max_retries=3, rate_limit='30/min')
 def qyer_detail_task(self, task, **kwargs):
     _sdk = QyerDetailSDK(task=task)
     return _sdk.execute()
