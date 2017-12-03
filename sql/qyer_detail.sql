9eb7c0cc131f013487f64a677553e17fa572feb6
nmghr9 <nmghr9@gmail.com>
2017-12-03 16:11:34 +0800

    修改表结构

--- sql/qyer_detail.sql
+++ sql/qyer_detail.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `%s` (
   `url` varchar(512) DEFAULT NULL,
   `phone` varchar(64) DEFAULT NULL,
   `site` varchar(256) DEFAULT NULL,
-  `imgurl` varchar(2048) DEFAULT NULL,
+  `imgurl` TEXT,
   `commenturl` varchar(512) DEFAULT NULL,
   `introduction` text,
   `opentime` varchar(1000) DEFAULT NULL,
