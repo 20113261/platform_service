3db1897c0805c5fe629b0952db39c4759cec7c48
nmghr9 <nmghr9@gmail.com>
2017-12-27 14:25:59 +0800

    test update

--- test/test_hotel_merge_img.py
+++ test/test_hotel_merge_img.py
@@ -6,6 +6,7 @@
 # @File    : test_hotel_merge_img.py
 # @Software: PyCharm
 import sys
+
 sys.path.append('/data/lib')
 from proj.my_lib.Common.Task import Task, TaskType
 from proj.total_tasks import hotel_img_merge_task
@@ -13,7 +14,7 @@ from proj.total_tasks import hotel_img_merge_task
 if __name__ == '__main__':
     task = Task(_worker='', _task_id='demo', _source='qyer', _type='poi_list', _task_name='test_task',
                 _used_times=0, max_retry_times=6,
-                kwargs={'uid': 'ht10391008',
+                kwargs={'uid': 'ht10015059',
                         'min_pixels': '200000'},
                 list_task_token='',
                 task_type=TaskType.NORMAL,
