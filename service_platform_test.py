671752f9614feb5e38b4f2d30349c05364cd48b0
nmghr9 <nmghr9@gmail.com>
2018-01-10 21:37:33 +0800

    列表页单元测试函数

---
+++ service_platform_test.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/10 上午10:20
+# @Author  : Hou Rong
+# @Site    :
+# @File    : service_platform_test.py
+# @Software: PyCharm
+import uuid
+import random
+import os
+import json
+import logging
+import functools
+import inspect
+import pymysql
+import pandas
+from DBUtils.PooledDB import PooledDB
+from mioji.common.task_info import Task
+from mioji.spider_factory import factory
+from datetime import datetime, timedelta
+from collections import defaultdict
+from logging.handlers import RotatingFileHandler
+
+log_path = "./"
+
+
+class NamedRotatingFileHandler(RotatingFileHandler):
+    def __init__(self, filename):
+        super(NamedRotatingFileHandler, self).__init__(
+            filename=os.path.join(log_path, "{0}.log".format(filename)),
+            maxBytes=100 * 1024 * 1024,
+            backupCount=2
+        )
+
+
+def get_logger(logger_name):
+    """
+    初始化 logger get 可以获取到，为单例模式
+    """
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+        os.mkdir(log_path)
+
+    # getLogger 为单例模式
+    service_platform_logger = logging.getLogger(logger_name)
+    service_platform_logger.setLevel(logging.DEBUG)
+    datefmt = "%Y-%m-%d %H:%M:%S"
+    file_log_format = "%(asctime)-15s %(threadName)s %(filename)s:%(lineno)d %(levelname)s:        %(message)s"
+    formtter = logging.Formatter(file_log_format, datefmt)
+
+    # handler 存在的判定
+    if not service_platform_logger.handlers:
+        # rotating file logger
+        file_handle = NamedRotatingFileHandler(logger_name)
+        file_handle.setFormatter(formtter)
+        service_platform_logger.addHandler(file_handle)
+        steam_handler = logging.StreamHandler()
+        service_platform_logger.addHandler(steam_handler)
+
+    return service_platform_logger
+
+
+func_count_dict = defaultdict(int)
+time_logger = get_logger('func_time_logger')
+
+
+def func_time_logger(fun):
+    @functools.wraps(fun)
+    def logging(*args, **kw):
+        try:
+            func_file = inspect.getfile(fun)
+        except Exception:
+            func_file = ''
+        func_name = fun.__name__
+        func_key = (func_file, func_name)
+        func_count_dict[func_key] += 1
+        begin = datetime.now()
+        result = fun(*args, **kw)
+        end = datetime.now()
+        time_logger.debug('[文件: {}][函数: {}][耗时 {}][当前运行 {} 个此函数]'.format(
+            func_file, func_name, end - begin, func_count_dict[func_key]
+        ))
+        func_count_dict[func_key] -= 1
+        return result
+
+    return logging
+
+
+def init_pool(host, user, password, database, max_connections=5):
+    mysql_db_pool = PooledDB(creator=pymysql, mincached=1, maxcached=2, maxconnections=max_connections,
+                             host=host, port=3306, user=user, passwd=password,
+                             db=database, charset='utf8', use_unicode=False, blocking=True)
+    return mysql_db_pool
+
+
+db_config = dict(
+    user='reader',
+    password='mioji1109',
+    host='10.10.230.206',
+    database='source_info'
+)
+spider_data_poi_pool = init_pool(**db_config)
+
+
+def fetchall(conn_pool, sql, is_dict=False):
+    conn = conn_pool.connection()
+    if is_dict:
+        cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
+    else:
+        cursor = conn.cursor()
+    cursor.execute('''SET SESSION sql_mode = (SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''));''')
+    cursor.execute(sql)
+    for line in cursor.fetchall():
+        yield line
+    cursor.close()
+    conn.close()
+
+
+def hotel_list_database(source, check_in, suggest_type='1', suggest=''):
+    # 初始化任务
+    task = Task()
+    task.ticket_info = {
+        "is_new_type": True,
+        "suggest_type": int(suggest_type),
+        "suggest": suggest,
+        "check_in": str(check_in),
+        "stay_nights": '1',
+        "occ": '2',
+        'is_service_platform': True,
+        'tid': uuid.uuid4(),
+        'used_times': random.randint(1, 6),
+    }
+    task.content = ''
+
+    # 初始化 spider
+    spider = factory.get_spider_by_old_source(source + 'ListHotel')
+    spider.task = task
+
+    # 请求
+    error_code = spider.crawl(required=['hotel'], cache_config=False)
+
+    return error_code, spider.result, spider.page_store_key_list
+
+
+def get_task(source):
+    sql = '''SELECT
+  sid,
+  suggest
+FROM ota_location
+WHERE source = '{}'
+ORDER BY rand()
+LIMIT 1;'''.format(source)
+    return list(fetchall(spider_data_poi_pool, sql=sql, is_dict=True))
+
+
+def test_crawl_result(source):
+    logger = get_logger(datetime.now().strftime('{}_test_%Y%m%d'.format(source)))
+    check_in = (datetime.now() + timedelta(days=20)).strftime('%Y%m%d')
+    tasks = get_task(source=source)
+
+    logger.info("[source: {}]".format(source))
+    logger.info("[check_in: {}]".format(check_in))
+    logger.info("[tasks: {}]".format(tasks))
+
+    data = []
+    for line in tasks:
+        error_code, result, page_store_key_list = hotel_list_database(source=source, check_in=check_in,
+                                                                      suggest=line['suggest'])
+        data.append({
+            'source': source,
+            'sid': line['sid'],
+            'suggest': json.dumps(line['suggest']),
+            'check_in': check_in,
+            'error_code': error_code,
+            'res_num': len(result['hotel']),
+            'result': json.dumps(result)
+        })
+
+    df = pandas.DataFrame(columns=['source', 'sid', 'suggest', 'check_in', 'error_code', 'res_num', 'result'],
+                          data=data)
+    df.to_excel(datetime.now().strftime('./{}_test_%Y%m%d.xlsx'.format(source)))
+
+
+if __name__ == '__main__':
+    test_crawl_result('booking')
