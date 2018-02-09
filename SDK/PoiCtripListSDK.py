6f43b1ef05e0a8f7c8d53783030793832b50017b
vonsago <vassagovon@von.local>
2018-02-09 16:22:20 +0800

    m

--- SDK/PoiCtripListSDK.py
+++ SDK/PoiCtripListSDK.py
@@ -94,7 +94,7 @@ class PoiCtripListSDK(BaseSDK):
         sql = SQL.format(self.task.task_name)
         data = []
         for sid, tag, url in result:
-            data.append(('ctripPOI', sid, city_id, country_id, url))
+            data.append(('ctripPoi', sid, city_id, country_id, url))
         try:
             service_platform_conn = service_platform_pool.connection()
             cursor = service_platform_conn.cursor()
@@ -121,11 +121,11 @@ if __name__ == '__main__':
         "city_id": "20645",
         "country_id": "133",
         "source": "ctripoi",
-        "city_url": "newyork248",
+        "city_url": "asti7060",
         "date_index": 0
     }
 
-    task = ttt(_worker='', _task_id='demo', _source='ctripPoi', _type='poi_list', _task_name='ctrip_poi_suggest',
+    task = ttt(_worker='', _task_id='demo', _source='ctripPoi', _type='poi_list', _task_name='list_qyer_total_test',
                 _used_times=0, max_retry_times=6,
                 kwargs=args, _queue='poi_list',
                 _routine_key='poi_list', list_task_token='test', task_type=0, collection='')
