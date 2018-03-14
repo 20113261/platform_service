ffb278bb4966f6172b4ead646b87e2d86dda1931
luwanning <luwanning@mioji.com>
2018-03-14 19:44:00 +0800

    suggest任务降速

--- proj/total_tasks.py
+++ proj/total_tasks.py
@@ -247,7 +247,7 @@ def google_drive_task(self, task, **kwargs):
     _sdk = GoogleDriveSDK(task=task)
     return _sdk.execute()
 
-@app.task(bind=True, base=BaseTask, max_retires=3, rate_limit='30/s')
+@app.task(bind=True, base=BaseTask, max_retires=3, rate_limit='10/s')
 def allhotel_city_suggest(self,task,**kwargs):
     _sdk = AllHotelSourceSDK(task=task)
     return _sdk.execute()
