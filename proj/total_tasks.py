d7c7557e8685df912adeccb07b9a4cd03ce7f97d
luwanning <luwanning@mioji.com>
2018-03-24 00:40:22 +0800

    图片任务降速

--- proj/total_tasks.py
+++ proj/total_tasks.py
@@ -136,7 +136,7 @@ def slow_hotel_detail_task(self, task, **kwargs):
 #
 #
 
-@app.task(bind=True, base=BaseTask, max_retries=2, rate_limit='40/s')
+@app.task(bind=True, base=BaseTask, max_retries=2, rate_limit='20/s')
 def images_task(self, task, **kwargs):
     _sdk = ImagesSDK(task=task)
     return _sdk.execute()
