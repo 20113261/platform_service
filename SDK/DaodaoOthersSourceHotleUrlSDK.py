c3a5a95dd0337c20ae0e535c6e9cca9030f6b0a9
luwanning <luwanning@mioji.com>
2018-03-29 10:54:44 +0800

    zhao bug

--- SDK/DaodaoOthersSourceHotleUrlSDK.py
+++ SDK/DaodaoOthersSourceHotleUrlSDK.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from proj.my_lib.Common.BaseSDK import BaseSDK
 import pymongo
-import types
 from mioji import spider_factory
 MONGODB_CONFIG = {
     'host': '10.10.213.148'
@@ -14,20 +12,20 @@ from mioji.spider_factory import factory
 from proj.my_lib import ServiceStandardError
 from proj.my_lib.Common.Browser import proxy_pool
 from celery.utils.log import get_task_logger
-import mioji.common.logger
-import mioji.common.pages_store
-import mioji.common.pool
+# import mioji.common.logger
+# import mioji.common.pages_store
+# import mioji.common.pool
 # mioji.common.pool.pool.set_size(128)
-logger = get_task_logger(__name__)
-mioji.common.logger.logger = loggergg
-mioji.common.pages_store.cache_dir = list_cache_path
-mioji.common.pages_store.STORE_TYPE = cache_type
-# 初始化工作 （程序启动时执行一次即可）
-insert_db = None
-# get_proxy = simple_get_socks_proxy
-get_proxy = proxy_pool.get_proxy
-debug = False
-spider_factory.config_spider(insert_db, get_proxy, debug, need_flip_limit=False)
+# logger = get_task_logger(__name__)
+# mioji.common.logger.logger = logger
+# mioji.common.pages_store.cache_dir = list_cache_path
+# mioji.common.pages_store.STORE_TYPE = cache_type
+# # 初始化工作 （程序启动时执行一次即可）
+# insert_db = None
+# # get_proxy = simple_get_socks_proxy
+# get_proxy = proxy_pool.get_proxy
+# debug = False
+# spider_factory.config_spider(insert_db, get_proxy, debug, need_flip_limit=False)
 
 def hotel_url_to_database(tid, used_times, source, keyword, spider_tag, need_cache=False):
     task = Task()
