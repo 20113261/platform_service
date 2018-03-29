0403a705ff2583713606c882191b1e25d91ea0f6
miojilx <2839255227@qq.com>
2018-03-29 19:04:35 +0800

    修改代码

--- SDK/DaodaoOthersSourceHotleUrlSDK.py
+++ SDK/DaodaoOthersSourceHotleUrlSDK.py
@@ -19,7 +19,7 @@ import mioji.common.pages_store
 import mioji.common.pool
 mioji.common.pool.pool.set_size(128)
 logger = get_task_logger(__name__)
-mioji.common.logger.logger = loggergg
+mioji.common.logger.logger = logger
 mioji.common.pages_store.cache_dir = list_cache_path
 mioji.common.pages_store.STORE_TYPE = cache_type
 # 初始化工作 （程序启动时执行一次即可）
@@ -35,9 +35,9 @@ def hotel_url_to_database(tid, used_times, source, keyword, spider_tag, need_cac
     spider = factory.get_spider_by_old_source(spider_tag)
     spider.task = task
     if need_cache:
-        error_code = spider.crawl(cache_config=cache_config)
+        error_code = spider.crawl(required=['hotel'], cache_config=cache_config)
     else:
-        error_code = spider.crawl(cache_config=none_cache_config)
+        error_code = spider.crawl(required=['hotel'], cache_config=none_cache_config)
     print(error_code)
     return error_code, spider.result
 
@@ -57,11 +57,26 @@ class OthersSourceHotelUrl(BaseSDK):
         )
         client = pymongo.MongoClient(**MONGODB_CONFIG)
         db = client['HotelUrl']['DaoDaoSourceUrl']
-        db.insert(values)
+        if values:
+            db.insert(values)
         if error_code == 0:
             self.finished_error_code = 0
         else:
             raise ServiceStandardError.ServiceStandardError(error_code,msg="爬虫出现错误")
 
 if __name__ == "__main__":
-    pass
+    from proj.my_lib.Common.Task import Task as Task_to
+    url = "https://www.tripadvisor.cn/Hotels-g1189702-Tahkovuori_Northern_Savonia-Hotels.html"
+    args = {
+        'url': url,
+        'source': 'daodao',
+        'spider_tag': 'daodaoListHotel'
+
+    }
+    task = Task_to(_worker='', _task_id='demo', _source='daodao', _type='suggest', _task_name='tes',
+               _used_times=0, max_retry_times=6,
+               kwargs=args, _queue='supplement_field',
+               _routine_key='supplement_field', list_task_token='test', task_type=0, collection='')
+    ihg = OthersSourceHotelUrl(task)
+
+    ihg.execute()
