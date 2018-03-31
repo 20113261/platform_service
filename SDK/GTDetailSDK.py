588b402baef64fdaa052eadae8c20caa27dda5bd
luwanning <luwanning@mioji.com>
2018-03-31 14:31:16 +0800

    使用NB代理

--- SDK/GTDetailSDK.py
+++ SDK/GTDetailSDK.py
@@ -16,14 +16,13 @@ import mioji.common.pool
 from celery.utils.log import get_task_logger
 from mioji import spider_factory
 from mioji.common.task_info import Task
-from mioji.common.utils import simple_get_socks_proxy
 from mioji.spider_factory import factory
 
 from proj.list_config import cache_config, list_cache_path, cache_type, none_cache_config
 from proj.my_lib.Common.BaseSDK import BaseSDK
 from proj.my_lib.ServiceStandardError import ServiceStandardError
-from proj.mysql_pool import service_platform_pool
-from proj.my_lib.Common.Browser import proxy_pool
+# from proj.my_lib.Common.Browser import proxy_pool
+from mioji.common.utils import simple_get_proxy
 
 mioji.common.pool.pool.set_size(128)
 logger = get_task_logger(__name__)
@@ -33,10 +32,9 @@ mioji.common.pages_store.STORE_TYPE = cache_type
 
 # 初始化工作 （程序启动时执行一次即可）
 insert_db = None
-# get_proxy = simple_get_socks_proxy
-get_proxy = proxy_pool.get_proxy
+# get_proxy = proxy_pool.get_proxy
 debug = False
-spider_factory.config_spider(insert_db, get_proxy, debug, need_flip_limit=False)
+spider_factory.config_spider(insert_db, simple_get_proxy, debug, need_flip_limit=False)
 SQL = "INSERT IGNORE INTO {} (source, source_id, city_id, country_id, hotel_url) VALUES (%s,%s,%s,%s,%s)"
 
 client = pymongo.MongoClient('mongodb://root:miaoji1109-=@10.19.2.103:27017/')
@@ -44,9 +42,6 @@ db = client['data_result']
 tuniu_collections = db['tuniuGT_detail']
 ctrip_collections = db['ctripGT_detail']
 
-# client = pymongo.MongoClient("mongodb://10.10.231.105:27017")
-# db = client['Test']
-# collections = db['ctrip_vacation001']
 
 def GTdetail_to_database(tid, used_times, source,ticket, need_cache=True):
     task = Task()
