d46300ba40e6d872a7b655bf17c92c0c35649e6b
vonsago <f811194414@gmail.com>
2018-04-02 14:41:50 +0800

    f

--- SDK/HotelListSDK.py
+++ SDK/HotelListSDK.py
@@ -157,7 +157,7 @@ class HotelListSDK(BaseSDK):
             self.task.error_code = error_code
 
         res_data = []
-        if source in ('ctrip', 'ctripcn', 'starwood', 'gha','shangrila'):
+        if source in ('ctrip', 'ctripcn', 'starwood', 'gha'):
             for line in result['hotel']:
                 sid = line[3]
                 hotel_url = line[-1]
