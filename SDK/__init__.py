6f2bc8e50074dd34e75f6feeef82e54842132edd
xiaoziqin <xiaoziqin@mioji.com>
2018-04-03 18:20:18 +0800

    .

--- SDK/__init__.py
+++ SDK/__init__.py
@@ -38,4 +38,4 @@ from .GoogleDriveSDK import GoogleDriveSDK
 from .AllSuggestCitySDK import AllSuggestCitySDK
 from .DaodaoOthersSourceHotleUrlSDK import OthersSourceHotelUrl
 from .DaodaoHotelDetailSDK import DaodaoHotelDetailSDK
-from .GoogleHotelSDK import GoogleHotelSDK
+from .GoogleHotelSDK import GoogleHotelUrl
