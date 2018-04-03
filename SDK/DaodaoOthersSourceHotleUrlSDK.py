a022890e078d875bdd5bb035e77fb9546a168f91
luwanning <luwanning@mioji.com>
2018-04-03 09:56:39 +0800

    修改发任务SDK

--- SDK/DaodaoOthersSourceHotleUrlSDK.py
+++ SDK/DaodaoOthersSourceHotleUrlSDK.py
@@ -1,30 +1,22 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import pymongo
-import pymysql
+# import pymongo
+# import pymysql
 from mioji import spider_factory
-MONGODB_CONFIG = {
-    'host': '10.10.213.148'
-}
-config = {
-    'host': '10.10.230.206',
-    'user': 'mioji_admin',
-    'password': 'mioji1109',
-    'db': 'daodao_google',
-    'charset': 'utf8'
-}
 import json
 from mioji.common.task_info import Task
 from proj.list_config import cache_config, list_cache_path, cache_type, none_cache_config
 from proj.my_lib.Common.BaseSDK import BaseSDK
+from proj.mysql_pool import service_platform_pool
+from proj.my_lib.logger import func_time_logger
 from mioji.spider_factory import factory
 from proj.my_lib import ServiceStandardError
 from proj.my_lib.Common.Browser import proxy_pool
 from celery.utils.log import get_task_logger
+# from toolbox import Common
 import mioji.common.logger
 import mioji.common.pool
 import mioji.common.pages_store
-from toolbox import Common
 mioji.common.pool.pool.set_size(128)
 
 logger = get_task_logger('daodaoHotel')
@@ -39,80 +31,76 @@ debug = False
 spider_factory.config_spider(insert_db, get_proxy, debug, need_flip_limit=False)
 
 
-def hotel_url_to_database(tid, used_times, source, keyword, spider_tag, need_cache=False,data_from = 'daodao'):
+def hotel_url_to_database(source, keyword, need_cache=False):
     task = Task()
     task.ticket_info['url'] = keyword
     task.ticket_info['hotel_name'] = keyword
-    spider = factory.get_spider_by_old_source(spider_tag)
+    old_target = source + 'ListHotel'
+    spider = factory.get_spider_by_old_source(old_target)
     spider.task = task
     if need_cache:
         error_code = spider.crawl(required=['hotel'], cache_config=cache_config)
     else:
         error_code = spider.crawl(required=['hotel'], cache_config=none_cache_config)
     print(error_code)
-    if data_from == 'google':
-        return error_code,spider.result,spider.user_datas['search_result']
-    return error_code, spider.result,''
+    # if data_from == 'google':
+    #     return error_code,spider.result,spider.user_datas['search_result']
+    print spider.result['hotel']
+    return error_code, spider.result['hotel']
 
 
 class OthersSourceHotelUrl(BaseSDK):
     def _execute(self, **kwargs):
         url = kwargs.get('url')
-        spider_tag = kwargs.get('spider_tag')
+        tag = kwargs.get('tag')
         source = kwargs.get('source')
-        data_from = kwargs.get('data_from')
-        error_code, values,search_result = hotel_url_to_database(
-            tid=self.task.task_id,
-            used_times=self.task.used_times,
-            spider_tag=spider_tag,
+        name = kwargs.get('name')
+        name_en = kwargs.get('name_en')
+        table_name = 'list_result_{0}_{1}'.format(source, tag)
+
+        error_code, hotel_result = hotel_url_to_database(
             source=source,
             keyword=url,
             need_cache=self.task.used_times == 0,
-            data_from=data_from
         )
         temp_save = []
 
-        if data_from == 'daodao':
-            hotel_urls = values['hotel']
-            insert_sql = "insert into daodao_hotel(hotel_name,hotel_name_en,agoda,booking,ctrip,elong,expedia,hotels,other_source) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-            conn = pymysql.connect(**config)
-            cursor = conn.cursor()
-            for hotel_url in hotel_urls:
-                temp_save.append(
-                    (
-                        hotel_url.get('hotel_name',''),hotel_url.get('hotel_name_en',''),hotel_url.get('agoda',''),hotel_url.get('booking',''),hotel_url.get('ctrip',''),
-                        hotel_url.get('elong',''),hotel_url.get('expedia',''),hotel_url.get('hotels',''),json.dumps(hotel_url)
-                    )
-                )
-                if len(temp_save) >= 2000:
-                    cursor.executemany(insert_sql,temp_save)
-                    conn.commit()
-                    temp_save = []
-            else:
-                cursor.executemany(insert_sql,temp_save)
-                conn.commit()
-        elif data_from == 'google':
-            hotel_urls = values['hotel']
-            insert_sql = "insert into google_hotel(hotel_name,hotel_name_en,agoda,booking,ctrip,elong,expedia,hotels,other_source) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-            conn = pymysql.connect(**config)
-            cursor = conn.cursor()
-            temp_dict = {}
-            hotel_name = hotel_name_en = ''
-            if ',' in url:
-                hotel_name,hotel_name_en = url.split(',')
-            else:
-                if Common.has_any(url,Common.is_chinese):
-                    hotel_name = url
-                else:
-                    hotel_name_en = url
-            for hotel_url in hotel_urls:
-                temp_dict = hotel_url
-            temp_save = (
-                hotel_name,hotel_name_en,temp_dict.get('agoda',''),temp_dict.get('booking',''),temp_dict.get('ctrip',''),temp_dict.get('elong',''),
-                temp_dict.get('expedia',''),temp_dict.get('hotels',''),json.dumps(search_result)
-            )
-            cursor.execute(insert_sql,temp_save)
-            conn.commit()
+        if source == 'daodao':
+            for hotel in hotel_result:
+                name = hotel.get('hotel_name', '')
+                name_en = hotel.get('hotel_name_en', '')
+                result = hotel.copy()
+                try:
+                    result.pop('hotel_name')
+                except:pass
+                try:
+                    result.pop('hotel_name_en')
+                except:pass
+                temp_save.append((name, name_en, 'daodao', 1, json.dumps(hotel) if hotel else None))
+
+        elif source == 'google':
+            for hotel in hotel_result:
+                status = 1 if hotel_result else 0
+                temp_save.append((name, name_en, 'google', status, json.dumps(hotel) if hotel else None))
+
+        print temp_save
+        @func_time_logger
+        def hotel_list_insert_db(table_name, res_data):
+            try:
+                service_platform_conn = service_platform_pool.connection()
+                cursor = service_platform_conn.cursor()
+                sql = "insert into {}(name, name_en, `from`, status, source_list) VALUES (%s,%s,%s,%s,%s)".format(table_name)
+                cursor.executemany(sql, res_data)
+                service_platform_conn.commit()
+                cursor.close()
+                service_platform_conn.close()
+            except Exception as e:
+                self.logger.exception(msg="[mysql error]", exc_info=e)
+                raise ServiceStandardError(error_code=ServiceStandardError.MYSQL_ERROR, wrapped_exception=e)
+
+        hotel_list_insert_db(table_name, temp_save)
+
+
         if error_code == 0:
             self.task.error_code = 0
         else:
@@ -121,12 +109,21 @@ class OthersSourceHotelUrl(BaseSDK):
 
 if __name__ == "__main__":
     from proj.my_lib.Common.Task import Task as Task_to
-    url = "Hotel Dukes' Palace"
+    # url = "https://www.tripadvisor.cn/Hotels-g1189702-Tahkovuori_Northern_Savonia-Hotels.html"
+    # args = {
+    #     'url': url,
+    #     'source': 'daodao',
+    #     'tag': '20180401a',
+    #     'name': 'test_chinese',
+    #     'name_en': 'test_english',
+    # }
+    url = "Domus Art Michelangelo"
     args = {
         'url': url,
-        'source': 'daodao',
-        'spider_tag': 'googlelistspider',
-        'data_from': 'google'
+        'source': 'google',
+        'tag': '20180401a',
+        'name': 'test_chinese',
+        'name_en': 'test_english',
     }
 
     task = Task_to(_worker='', _task_id='demo', _source='daodao', _type='suggest', _task_name='tes',
