cb110f7d89a66d347ee8539ba6fbd1f367d07754
luwanning <luwanning@mioji.com>
2018-04-03 12:11:16 +0800

    修改答案及抓取表结构

--- sql/result_hotel.sql
+++ sql/result_hotel.sql
@@ -3,10 +3,12 @@ CREATE TABLE IF NOT EXISTS `%s` (
   `from` varchar(24) NOT NULL,
   `name` varchar(1024) DEFAULT NULL,
   `name_en` varchar(1024) DEFAULT NULL,
+  `city_id` varchar(45) DEFAULT NULL,
+  `country_id` varchar(45) DEFAULT NULL,
   `status` tinyint(4) NOT NULL DEFAULT '0',
   `source_list` json DEFAULT NULL,
   `utime` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   PRIMARY KEY (`id`),
   KEY `source` (`from`),
   KEY `utime` (`utime`)
-) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4
