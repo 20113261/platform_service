f00119a78034ac4c79871d505380338c219e3b6f
luwanning <luwanning@mioji.com>
2018-04-05 21:09:19 +0800

    test

--- rabbitmq_watcher.py
+++ rabbitmq_watcher.py
@@ -141,7 +141,7 @@ TASK_CONF = {
     # 'supplement_field': (9000, 40000, 10),
     # 'google_api': (9000, 40000, 10),
     # 'merge_task': (10000, 40000, 11),
-    'grouptravel': (40000, 60000, 10)
+    'grouptravel': (36000, 40000, 5)
 }
 
 MAX_RETRY_TIMES_CONF = {
