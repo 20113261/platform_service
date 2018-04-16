e7420d7262c6d51913d6059ceed720008c034883
zhanglaimeng <zhanglaimeng@mioji.com>
2018-04-16 09:35:38 +0800

    修改booking解析

--- proj/my_lib/hotel_parse_new/new_booking_parser.py
+++ proj/my_lib/hotel_parse_new/new_booking_parser.py
@@ -6,8 +6,8 @@ import requests
 import json
 from lxml import html as HTML
 # from data_obj import BookingHotel
-# from mioji.common.class_common import Hotel_New
-from proj.my_lib.models.HotelModel import HotelNewBase as Hotel_New
+from mioji.common.class_common import Hotel_New
+# from proj.my_lib.models.HotelModel import HotelNewBase as Hotel_New
 from proj.my_lib.models.HotelModel import HotelNewBase
 # from common.common import get_proxy
 # from proj.my_lib.Common.KeyMatch import key_is_legal
@@ -57,7 +57,7 @@ def booking_parser(content, url, other_info):
         print('Parser ERROR, NO Traffic Infomation.The reason follows: %s' % e.message)
     # hotel.chiled_bed_type = '\n'.join(root.xpath('//*[@id="children_policy"]/p[position()>1]/text()'))
     hotel.chiled_bed_type = ''.join(
-        [i.replace('\n', '').strip() for i in root.xpath('//*[@id="children_policy"]/p[position()>1]//text()') if
+        [i.replace('\n', '').strip() for i in root.xpath('//*[@id="children_policy"]/p[position()>1]//text()|//*[@id="general-child-policy"]/p[position()>1]//text()') if
          i.replace('\n', '').strip()])
     hotel.pet_type = ''.join([i.replace('\n', '').strip() for i in root.xpath(
         '//*[@id="hotelPoliciesInc"]/div[@class="description"]/p[position()>1]//text()') if
@@ -78,21 +78,26 @@ def booking_parser(content, url, other_info):
     hotel.facility_content['Parking'] = parking
 
     # 设施新字段添加到facilities_dict， 即可自动匹配
-    facilities_dict = {'Swimming_Pool': '游泳池', 'gym': '健身房', 'SPA': 'SPA', 'Bar': '酒吧', 'Coffee_house': '咖啡厅',
-                       'Tennis_court': '网球场', 'Golf_Course': '高尔夫球场', 'Sauna': '桑拿', 'Mandara_Spa': '水疗中心',
-                       'Recreation': '儿童娱乐场', 'Business_Centre': '商务中心', 'Lounge': '行政酒廊',
-                       'Wedding_hall': '婚礼礼堂', 'Restaurant': '餐厅',
-                       'Airport_bus': '机场班车', 'Valet_Parking': '代客泊车', 'Call_service': '叫车服务', 'Rental_service': '租车服务'}
+    facilities_dict = {'Swimming_Pool': ['游泳池'], 'gym': ['健身房'], 'SPA': ['SPA'], 'Bar': ['酒吧'], 'Coffee_house': ['咖啡厅'],
+                    'Tennis_court': ['网球场'], 'Golf_Course': ['高尔夫球场'], 'Sauna': ['桑拿'], 'Mandara_Spa': ['水疗中心'],
+                    'Recreation': ['儿童娱乐场', '儿童游乐场'], 'Business_Centre': ['商务中心'], 'Lounge': ['行政酒廊'],
+                    'Wedding_hall': ['婚礼礼堂'], 'Restaurant': ['餐厅'],
+                    'Airport_bus': ['机场班车', '班车服务', '班车服务(收费)'], 'Valet_Parking': ['代客泊车'], 'Call_service': ['叫车服务'], 'Rental_service': ['租车服务']}
     part_facilities = map(lambda x: x.encode('utf-8').replace('\n', '').strip(), root.xpath(
         '//*[@id="hp_facilities_box"]/div[@class="facilitiesChecklist"]/div/ul/li/span[@data-name-en]/text()'))
     parser_list = []
-    reverse_facility_dict = {v: k for k, v in facilities_dict.items()}
+    # reverse_facility_dict = {v: k for k, v in facilities_dict.items()}
+    # print reverse_facility_dict
     for every in part_facilities:
         value = every.replace('咖啡', '咖啡厅').replace('网球', '网球场').replace('健身', '健身房').replace('儿童娱乐', '儿童游乐').upper()
-        for faci in facilities_dict.values():
-            if faci in value:
-                hotel.facility_content[reverse_facility_dict[faci]] = every
-                parser_list.append(every)
+        for keys, faci in facilities_dict.items():
+            for fac in faci:
+                if fac in value:
+                    if keys in hotel.facility_content:
+                        hotel.facility_content[keys] += every
+                    else:
+                        hotel.facility_content[keys] = every
+                    parser_list.append(every)
     print('酒店设施：{}'.format(', '.join(part_facilities)))
     print('已解析出：%s' % ', '.join(parser_list))
     service_list = map(lambda x: x.encode('utf-8').replace('\n', '').strip(),
@@ -116,9 +121,14 @@ def booking_parser(content, url, other_info):
     print('酒店服务：{}'.format(', '.join(service_list) or '如果你看见了这句话请不要好奇，它表示酒店服务项目是空的'))
     print('已解析出：%s' % ', '.join(parser_sevice_list))
     hotel.img_items = '|'.join(root.xpath('//*[@id="photos_distinct"]/a[position()<last()-1]/@href'))
+    if not hotel.img_items:
+        hotel.img_items = '|'.join(root.xpath('//div[@class="bh-photo-grid-thumb-cell"]/a/@href'))
     hotel.description = '\n'.join(
         map(lambda x: x.strip(), root.xpath('//*[@id="summary"]/p/text()')))
-    hotel.accepted_cards = '|'.join(root.xpath('//*[@class="jq_tooltip payment_methods_overall"]/button/@aria-label'))
+    a = root.xpath('//*[@class="jq_tooltip payment_methods_overall"]/button/@aria-label|'
+                                               '//div[contains(@class, "payment_promotion_labels")]/label/span/text()')
+
+    hotel.accepted_cards = '|'.join(a)
     hotel.check_in_time = re.sub(pattern=r'<script.+?script>', repl='',
                                  string=root.xpath('//*[@id="checkin_policy"]/p/span/@data-caption')[0].encode('utf-8'),
                                  flags=re.S).strip()
@@ -126,6 +136,7 @@ def booking_parser(content, url, other_info):
                                   string=root.xpath('//*[@id="checkout_policy"]/p/span/@data-caption')[0].encode(
                                       'utf-8'), flags=re.S).strip()
     hotel.hotel_url = url.encode('utf-8')
+    print json.dumps(hotel.to_dict(), ensure_ascii=False)
     return hotel.to_dict()
 
 
@@ -135,6 +146,8 @@ if __name__ == '__main__':
     monkey.patch_all()
     from gevent.queue import Queue
     import gevent
+    from fake_useragent import UserAgent
+    ua = UserAgent()
 
 
     def list_to_queue(_):
@@ -145,25 +158,41 @@ if __name__ == '__main__':
     start = time()
     other_info = {'source_id': '1016533', 'city_id': '10067'}
     queue = Queue()
-    url_list = (i for i in open(r'/Users/mioji/Desktop/booking_detail_url.txt', 'r'))
+    url_list = (i for i in open(r'/Users/mioji/work/work_place/booking_task.txt', 'r'))
+    # print url_list, '-----'
+    url_list = [
+
+        'http://www.booking.com/hotel/nl/salon-la-haye.zh-cn.html?aid=376390;label=misc-aHhSC9cmXHUO1ZtqOcw05wS94870954985%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap1t1%3Aneg%3Afi%3Atikwd-11455299683%3Alp9061505%3Ali%3Adec%3Adm;sid=cf33ef00c3ac1c507f69fca9dbfcdbe8;checkin=2018-05-01;checkout=2018-05-02;ucfs=1;aer=1;srpvid=42bd1612c0660471;srepoch=1521601702;highlighted_blocks=100004801_101902367_2_0_0;all_sr_blocks=100004801_101902367_2_0_0;bshb=2;room1=A%2CA;hpos=10;hapos=205;dest_type=city;dest_id=-2147114;srfid=af747ee3d1c054c692eff026b7c1e77d5096aab2X205;from=searchresults;highlight_room=;spdest=ci/-2147114;spdist=22.7#hotelTmpl'
+    ]
     gevent.spawn(list_to_queue, url_list).join()
     print(queue.qsize())
 
     def f(que):
+        # print '抓取进来'
         while not que.empty():
+            # print '循环来'
             url = que.get_nowait()
+            url = url.replace('\\n', '')
+            url_index = url.split(';')[0]
+            # print url
             while True:
+                # print '请求钱'
                 try:
-                    content = requests.get(url).text
+                    session = requests.session()
+                    user_agent = ua.random
+                    session.get(url_index, headers={'User-Agent': user_agent})
+                    content = session.get(url, headers={'User-Agent': user_agent}).text
                 except:
                     pass
                 else:
                     break
-            print(url)
+            # print(url)
             result = booking_parser(content, url, other_info)
             print(result)
             print('- ' * 100)
 
-    gevent.joinall([gevent.spawn(f, queue) for i in range(10)])
+    print '开始抓取'
+    gevent.joinall([gevent.spawn(f, queue) for i in range(1)])
+    print '33'
     end = time()
     print('Total time is %.2f s' % (end - start))
