336012591572b5874d41c5335ac4d5d35c13e1a9
luwanning <luwanning@mioji.com>
2018-04-16 14:22:42 +0800

    任务降速

--- proj/total_tasks.py
+++ proj/total_tasks.py
@@ -91,7 +91,7 @@ def hotel_img_merge_task(self, task, **kwargs):
 #
 
 
-@app.task(bind=True, base=BaseTask, max_retries=3, rate_limit='5/s')
+@app.task(bind=True, base=BaseTask, max_retries=3, rate_limit='3/s')
 def hotel_list_task(self, task, **kwargs):
     _sdk = HotelListSDK(task=task)
     return _sdk.execute()
@@ -121,7 +121,7 @@ def poi_detail_task(self, task, **kwargs):
     return _sdk.execute()
 
 
-@app.task(bind=True, base=BaseTask, max_retries=2, rate_limit='5/s')
+@app.task(bind=True, base=BaseTask, max_retries=2, rate_limit='25/m')
 def hotel_detail_task(self, task, **kwargs):
     _sdk = HotelDetailSDK(task=task)
     return _sdk.execute()
