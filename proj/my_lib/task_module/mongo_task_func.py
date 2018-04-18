0e1a3f7ccaaac2c5154e273ddeece35855c16042
luwanning <luwanning@mioji.com>
2018-04-18 14:51:55 +0800

    暂停任务

--- proj/my_lib/task_module/mongo_task_func.py
+++ proj/my_lib/task_module/mongo_task_func.py
@@ -64,6 +64,7 @@ def get_task_total_simple(queue, used_times=6, limit=30000, debug=False):
     for each_collection_name, each_limit in c_list:
 
         if each_collection_name=='Task_Queue_hotel_list_TaskName_google_hotel_url_20180409a':continue
+        if each_collection_name.endswith('20180412g'): continue
         if each_collection_name.endswith('20180416a'):continue
         # if each_collection_name!='Task_Queue_hotel_list_TaskName_list_result_daodao_20180412a':continue
         # if each_collection_name=='Task_Queue_grouptravel_TaskName_detail_total_GT_20180314a': continue
