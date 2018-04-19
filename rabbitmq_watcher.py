fc0d7efa416ea0dd73558553e2d3faae01c3d3bc
zhangxiaopeng <>
2018-04-19 17:23:38 +0800

    增加希尔顿

--- rabbitmq_watcher.py
+++ rabbitmq_watcher.py
@@ -81,10 +81,23 @@ hotel_slow_source = {
                 'slow_hotel_list',
                 'slow_hotel_list'
             )
+        },
+    'hilton':
+        {
+            'proj.total_tasks.hotel_detail_task': (
+                'proj.total_tasks.hilton_detail_task',
+                'slow_hotel_detail',
+                'slow_hotel_detail'
+            ),
+            'proj.total_tasks.hotel_list_task': (
+                'proj.total_tasks.hilton_list_task',
+                'slow_hotel_list',
+                'slow_hotel_list'
+            )
         }
 }
 
-slow_source = 'ihg|holiday|accor|marriott'
+slow_source = 'ihg|holiday|accor|marriott|hilton'
 
 import datetime
 
